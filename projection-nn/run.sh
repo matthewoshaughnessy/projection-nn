ffa2ca34259089f404780062b4ef4fe1b55e4a62
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-08-19 20:36:45 -0400

    update run script from example

---
+++ projection-nn/run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "python3 main.py"
+python3 main.py 2>&1 | tee main_log.txt
--- projection-resnet/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "python -u trainer.py  --arch=resnet20  --save-dir=save_resnet20 2>&1 | tee -a log_resnet20"
-python -u trainer.py  --arch=resnet20  --save-dir=save_resnet20 2>&1 | tee -a log_resnet20
