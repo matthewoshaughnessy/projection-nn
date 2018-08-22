d9d167b0e10270428d7efb85c1b18772ef516494
Matt O'Shaughnessy <matthewoshaughnessy@gatech.edu>
2018-08-22 17:15:13 -0400

    adding cuda disabled mode

--- projection-nn/main.py
+++ projection-nn/main.py
@@ -15,48 +15,53 @@ import torch.optim.lr_scheduler
 import torch.utils.data
 import torchvision.datasets as datasets
 
-import resnet
 import util
 import linearIneqTestData
+
+
+args = {'d'        : 2,
+        'n'        : 64,
+        'randseed' : 5,
+        'nEpochs'  : 500,
+        'Ktrain'   : 256,
+        'Kval'     : 100,
+        'Ktest'    : 100,
+        'useCuda'  : False}
 
 
 def main():
 
     # parameters
-    d = 2
-    n = 64
-    randseed = 5
-    nEpochs = 20
-    Ktrain = 256
-    Kval = 100
-    Ktest = 100
+    args['useCuda'] = torch.cuda.is_available()
+    print('CUDA enabled: {0:}'.format(args['useCuda']))
 
     # seed rng
-    np.random.seed(randseed)
+    np.random.seed(args['randseed'])
 
     # --- generate inequalities to make convex set ---
     print('Making data...')
-    ineq = linearIneqTestData.makeData(d, n, randseed)
+    ineq = linearIneqTestData.makeData(args['d'], args['n'], args['randseed'])
     print('done.')
 
     # --- generate point/projected point pairs ---
     print('Making training/validation/test sets:')
     print('Training set...')
-    dataTrain = linearIneqTestData.makePointProjectionPairs(ineq, Ktrain)
+    dataTrain = linearIneqTestData.makePointProjectionPairs(ineq, args['Ktrain'])
     trainDataset = ProjectionDataset(dataTrain['P'], dataTrain['Pproj'])
     print('Validation set...')
-    dataVal = linearIneqTestData.makePointProjectionPairs(ineq, Kval)
+    dataVal = linearIneqTestData.makePointProjectionPairs(ineq, args['Kval'])
     valDataset = ProjectionDataset(dataVal['P'], dataVal['Pproj'])
     print('Test set...')
-    dataTest = linearIneqTestData.makePointProjectionPairs(ineq, Ktest)
+    dataTest = linearIneqTestData.makePointProjectionPairs(ineq, args['Ktest'])
     testDataset = ProjectionDataset(dataTest['P'], dataVal['Pproj'])
     print('done.')
     #linearIneqTestData.plot(ineq, data['P'], data['Pproj'])
 
     # --- train network ---
     print('Constructing network...')
-    model = Network(d)
-    model.cuda()
+    model = Network()
+    if args['useCuda']:
+        model.cuda()
     print(model)
     print('done.')
 
@@ -66,11 +71,14 @@ def main():
                                 weight_decay=1e-4)
     lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer,
                                                         milestones=[30, 80])
-    criterion = torch.nn.SmoothL1Loss().cuda() # huber loss
+    if args['useCuda']:
+        criterion = torch.nn.SmoothL1Loss().cuda() # huber loss
+    else:
+        criterion = torch.nn.SmoothL1Loss()
     print('done.')
 
     print('Training...')
-    for epoch in range(nEpochs):
+    for epoch in range(args['nEpochs']):
 
         # train one epoch
         currentLR = optimizer.param_groups[0]['lr']
@@ -80,9 +88,9 @@ def main():
         # evaluate on validation set
         avgValLoss = validate(valDataset, model, criterion)
         print('Epoch {0:d}/{1:d}\tlr = {2:.5e}\tmean l2 err = {3:.7f}'.format(
-            epoch+1, nEpochs, currentLR, avgValLoss))
+            epoch+1, args['nEpochs'], currentLR, avgValLoss))
 
-    print('Training ({0:d} epochs) complete!'.format(nEpochs))
+    print('Training ({0:d} epochs) complete!'.format(args['nEpochs']))
 
     # --- save results on training/eval set ---
     print('Saving results...')
@@ -114,12 +122,12 @@ class Network(nn.Module):
         Defines the simple network that will perform projections
     """
 
-    def __init__(self, d):
+    def __init__(self):
         super(Network, self).__init__()
-        self.fc1 = torch.nn.Linear(d,2*d)
-        self.fc2 = torch.nn.Linear(2*d,4*d)
-        self.fc3 = torch.nn.Linear(4*d,2*d)
-        self.fc4 = torch.nn.Linear(2*d,d)
+        self.fc1 = torch.nn.Linear(args['d'],2*args['d'])
+        self.fc2 = torch.nn.Linear(2*args['d'],4*args['d'])
+        self.fc3 = torch.nn.Linear(4*args['d'],2*args['d'])
+        self.fc4 = torch.nn.Linear(2*args['d'],args['d'])
 
     def forward(self, x):
         x = torch.nn.functional.relu(self.fc1(x))
@@ -146,8 +154,12 @@ def train(trainDataset, model, criterion, optimizer):
         pproj = sample['pproj']
 
         # send to gpu
-        input_var = torch.autograd.Variable(p).cuda().float()
-        target_var = torch.autograd.Variable(pproj).cuda().float()
+        if args['useCuda']:
+            input_var = torch.autograd.Variable(p).cuda().float()
+            target_var = torch.autograd.Variable(pproj).cuda().float()
+        else:
+            input_var = torch.autograd.Variable(p).float()
+            target_var = torch.autograd.Variable(pproj).float()
 
         # compute output
         output = model(input_var)
@@ -161,7 +173,7 @@ def train(trainDataset, model, criterion, optimizer):
         output = output.float()
         loss = loss.float()
         # measure accuracy and record loss
-        lossAvg.update(loss.data.item())
+        lossAvg.update(loss.data[0])
 
 
 def validate(valDataset, model, criterion):
@@ -179,11 +191,17 @@ def validate(valDataset, model, criterion):
         sample = valDataset[i]
         p = sample['p']
         pproj = sample['pproj']
-        p_input = torch.autograd.Variable(p).cuda().float()
+        if args['useCuda']:
+            p_input = torch.autograd.Variable(p).cuda().float()
+        else:
+            p_input = torch.autograd.Variable(p).float()
 
         # compute output
-        pproj_hat = model(p_input).cpu()
-        l2err_avg.update(np.linalg.norm(pproj.numpy() - pproj_hat.detach().numpy()))
+        if args['useCuda']:
+            pproj_hat = model(p_input).cpu()
+        else:
+            pproj_hat = model(p_input).data
+        l2err_avg.update(np.linalg.norm(pproj.numpy() - pproj_hat.numpy()))
 
     return l2err_avg.get()
 
@@ -206,16 +224,25 @@ def saveTestResults(dataset, model, filename):
         sample = dataset[i]
         p = sample['p']
         pproj = sample['pproj']
-        p_input = torch.autograd.Variable(p).cuda().float()
+        if args['useCuda']:
+            p_input = torch.autograd.Variable(p).cuda().float()
+        else:
+            p_input = torch.autograd.Variable(p).float()
 
         # compute output
-        pproj_hat = model(p_input).cpu()
-        l2err = np.linalg.norm(pproj.numpy() - pproj_hat.detach().numpy())
+        if args['useCuda']:
+            pproj_hat = model(p_input).cpu().detach()
+        else:
+            pproj_hat = model(p_input).data
+        l2err = np.linalg.norm(pproj.numpy() - pproj_hat.numpy())
 
         # store
         P[:,i] = p
         Pproj[:,i] = pproj
-        Pproj_hat[:,i] = pproj_hat.detach().numpy()
+        if args['useCuda']:
+            Pproj_hat[:,i] = pproj_hat.detach().numpy()
+        else:
+            Pproj_hat[:,i] = pproj_hat.numpy()
         errs[i] = l2err
 
     # save
